ab4c16c206fc84501309ed4d8f2d602d50e15a77
Yixin Zhang <m.yixin.zhang@gmail.com>
2015-04-06 19:38:23 -0400

    API: Added comparison operators
    Included overloaded comparison operators: ==, !=, <, <=, >, >=

--- bignum.h
+++ bignum.h
@@ -10,18 +10,26 @@ class BigNum {
     public:
         BigNum();
         BigNum(const BigNum &rhs);
-        BigNum& operator=(const BigNum &rhs);
         BigNum(int n);
         BigNum(string n);
         ~BigNum();
         void print() const;
+        int getDigits() const;
 
-        BigNum& operator++();
-        BigNum operator++(int);
-        const BigNum& operator--();
-        const BigNum& operator--(int);
+        BigNum& operator=(const BigNum &rhs);
+        bool operator==(const BigNum &rhs) const;
+        bool operator!=(const BigNum &rhs) const;
+        bool operator<(const BigNum &rhs) const;
+        bool operator>(const BigNum &rhs) const;
+        bool operator<=(const BigNum &rhs) const;
+        bool operator>=(const BigNum &rhs) const;
+        BigNum& operator++();   // Pre-increment
+        BigNum operator++(int); // Post-increment
+        BigNum& operator--();   // Pre-decrement
+        BigNum operator--(int);// Post-decrement
         BigNum& operator+=(const BigNum &rhs);
         BigNum& operator-=(const BigNum &rhs);
+        BigNum operator+();
         BigNum operator-();
         BigNum operator+(const BigNum &rhs);
         BigNum operator-(const BigNum &rhs);
@@ -30,10 +38,11 @@ class BigNum {
 
     private:
         bool isValidNumber(string n) const;
+        static const unsigned int DefaultBase;
         inline int dp() const;  // Decimal places
-        bool negative;
+        bool neg;               // Negative
         vector<char> sig;       // Significand
-        const static int base;
+        int base;
         int exp;                // Exponent
 };
 
