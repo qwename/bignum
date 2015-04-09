53f0e36d1b916a7e65d7a296848df5b2dc4ca06d
Yixin Zhang <m.yixin.zhang@gmail.com>
2015-04-08 23:18:15 -0400

    API: Added Static Variable For Precision
    Added variable to set precision (maximum number of decimal places).

--- bignum.h
+++ bignum.h
@@ -13,8 +13,9 @@ class BigNum {
         BigNum(int n);
         BigNum(string n);
         ~BigNum();
-        friend void print(const BigNum &);
         int getDigits() const;
+        static void SetPrecision(int);
+        friend void print(const BigNum &);
         friend string toStr(const BigNum &);
         friend BigNum Floor(const BigNum &);    // Integer digits
         friend BigNum Fract(const BigNum &);    // Fractional digits
@@ -47,16 +48,20 @@ class BigNum {
         void alignDigits(const BigNum &);
         int floorDigits() const;
         int fractDigits() const;
+        bool floorZero() const;
         void shiftLeft(unsigned);
         void shiftRight(unsigned);
         static int compareMagnitude(const BigNum &, const BigNum &);
         static void throwInvalidNumber(const string &);
+        static void printDebug(const BigNum &);
 
-        static const unsigned int DefaultBase;
+        static const int DefaultBase;
+        static const int DefaultPrecision;
         bool neg;               // Negative
         vector<char> sig;       // Significand
         int base;
         int exp;                // Exponent
+        static int precision;
 };
 
 #endif
