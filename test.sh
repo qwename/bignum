caa569416eff051762294f5257178b5b3dbcc37d
Yixin Zhang <m.yixin.zhang@gmail.com>
2015-04-28 18:31:11 -0400

    Included basic parameter parsing
    If "debug" or nothing is passed in, the program is compiled with
    debug options. Pass in any other parameter to disable this.

--- test.sh
+++ test.sh
@@ -1,1 +1,10 @@
-make && bin/test < data
+if [ "$#" -eq 1 ]; then
+    if [ "$1" == "debug" ]; then
+        make DEBUG=1 && bin/debug/test < data
+    else
+        make && bin/test < data
+    fi
+else
+    make DEBUG=1 && bin/debug/test < data
+fi
+
