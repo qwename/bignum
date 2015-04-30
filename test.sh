ac37bb90804070f721d8f9c5049f6f2291c5e9d0
Yixin Zhang <m.yixin.zhang@gmail.com>
2015-04-29 23:34:31 -0400

    Allowed for passing in number to calculate factorial

--- test.sh
+++ test.sh
@@ -1,15 +1,21 @@
 g++ -c -std=c++11 -Wall test.cc -o test.o
-g++ test.o bin/bignum.a -o test
+if [ "$?" -ne 0 ]; then
+    exit
+fi
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -ge 1 ]; then
     if [ "$1" == "debug" ]; then
-        OPTBIN := 
-        make DEBUG=1 && ./test < data
+        make DEBUG=1 && \
+        g++ test.o bin/debug/bignum.a -o test && \
+        ./test $2 < data
     else
-        make && ./test < data
+        make && \
+        g++ test.o bin/bignum.a -o test && \
+        ./test $1 < data
     fi
 else
-    make DEBUG=1
+    make DEBUG=1 && \
+    g++ test.o bin/debug/bignum.a -o test && \
     ./test < data
 fi
 
